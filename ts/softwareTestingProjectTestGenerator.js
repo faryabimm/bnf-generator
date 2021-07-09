ca34119feae6eb01fd0f90e0eadbe1b75dbab5a0
Mohammadmahdi Faryabi <faryabimm@gmail.com>
2021-07-09 12:19:09 +0430

    Add npm run task and refactor

--- ts/softwareTestingProjectTestGenerator.js
+++ ts/softwareTestingProjectTestGenerator.js
@@ -1,11 +1,12 @@
-let BNFController = require('./BNFController').default;
-let readFileSync = require('fs').readFileSync;
-let path = require('path');
+const BNFController = require('./BNFController').default;
+const { readFileSync, writeFileSync } = require('fs');
+const path = require('path');
 
 const bnf = BNFController.getInstance();
 const sourceCode = readFileSync(path.join(__dirname, 'miniJava.bnf'), 'utf-8');
 bnf.bnfsubmitted(sourceCode);
-console.log(bnf.onGenerate());
+const generatedSample = bnf.onGenerate()
+console.log(generatedSample);
 
 
 
